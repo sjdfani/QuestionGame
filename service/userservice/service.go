81544e72724c09d30abb1b6484cc873d40941893
Sajjad fani <faniam321@gmail.com>
2025-10-27 00:46:14 +0330

    add login service and update register service to get and save password

--- service/userservice/service.go
+++ service/userservice/service.go
@@ -5,11 +5,14 @@ import (
 	"QuestionGame/pkg/phonenumber"
 
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Repository interface {
 	IsPhoneNumberUnique(phonenumber string) (bool, error)
 	Register(u entity.User) (entity.User, error)
+	GetUserByPhoneNumber(phonenumber string) (entity.User, bool, error)
 }
 
 type Service struct {
@@ -17,12 +20,17 @@ type Service struct {
 }
 
 type RegisterRequest struct {
-	Name        string
-	PhoneNumber string
+	Name        string `json:"name"`
+	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
 }
 
 type RegisterResponse struct {
 	User entity.User
+}
+
+func New(repo Repository) Service {
+	return Service{repo: repo}
 }
 
 func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
@@ -49,10 +57,22 @@ func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 		return RegisterResponse{}, fmt.Errorf("name length should be greater than 3")
 	}
 
+	// TODO: Check the password with regex pattern
+	// validate password
+	if len(req.Password) < 8 {
+		return RegisterResponse{}, fmt.Errorf("password length should be greater than 8")
+	}
+	bytePassword := []byte(req.Password)
+	hashedPassword, err := bcrypt.GenerateFromPassword(bytePassword, 0)
+	if err != nil {
+		return RegisterResponse{}, fmt.Errorf("there is problem in hashing password: %v\n", err)
+	}
+
 	user := entity.User{
 		ID:          0,
 		Name:        req.Name,
 		PhoneNumber: req.PhoneNumber,
+		Password:    string(hashedPassword),
 	}
 
 	// create new user in storage
@@ -63,4 +83,39 @@ func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 
 	// return created user
 	return RegisterResponse{User: created_user}, nil
+}
+
+type LoginRequest struct {
+	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
+}
+
+type LoginResponse struct {
+}
+
+func (s Service) Login(req LoginRequest) (LoginResponse, error) {
+	// TODO - it would be better to separate check existence and get user into two functions
+
+	// get user and check existence
+	user, exists, err := s.repo.GetUserByPhoneNumber(req.PhoneNumber)
+	if err != nil {
+		return LoginResponse{}, fmt.Errorf("unexpected error: %w", err)
+	}
+	if !exists {
+		return LoginResponse{}, fmt.Errorf("username or password is incorrect")
+	}
+
+	// hash request password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), 0)
+	if err != nil {
+		return LoginResponse{}, fmt.Errorf("there is problem in hashing password: %v\n", err)
+	}
+
+	// compare password and user's password
+	if string(hashedPassword) != user.Password {
+		return LoginResponse{}, fmt.Errorf("username or password is incorrect")
+	}
+
+	// return ok
+	return LoginResponse{}, nil
 }
