bfa0671b37f48a86e93942edbf41a204baace9f6
Sajjad fani <faniam321@gmail.com>
2025-10-30 21:47:15 +0330

    update main and remove extra functions

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
+	"QuestionGame/config"
+	"QuestionGame/delivery/httpserver"
 	"QuestionGame/repository/mysql"
 	"QuestionGame/service/authservice"
 	"QuestionGame/service/userservice"
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"time"
 )
 
@@ -21,121 +18,35 @@ const (
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/users/register", registerHandler)
-	mux.HandleFunc("/users/login", userLoginHandler)
-	mux.HandleFunc("/users/profile", userProfileHandler)
+	cfg := config.Config{
+		HTTPServer: config.HTTPServer{Port: 8080},
+		Auth: authservice.Config{
+			SignKey:               JWTSignKey,
+			AccessSubject:         AccessTokenSubject,
+			RefreshSubject:        RefreshTokenSubject,
+			AccessExpirationTime:  AccessTokenDuration,
+			RefreshExpirationTime: RefreshTokenDuration,
+		},
+		Mysql: mysql.MysqlConfig{
+			Host:     "127.0.0.1",
+			Port:     3308,
+			User:     "root",
+			Password: "root_password",
+			DBName:   "question_game_db",
+		},
+	}
 
-	log.Println("Server is listening on port 8080 ...")
+	authSvc, userSvc := setupServices(cfg)
+	server := httpserver.New(cfg, authSvc, userSvc)
 
-	http.ListenAndServe(":8080", mux)
+	server.Serve()
 }
 
-func registerHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, `{"detail": "Method not allowed"}`, http.StatusMethodNotAllowed)
-		return
-	}
+func setupServices(cfg config.Config) (authservice.Service, userservice.Service) {
+	authSvc := authservice.New(cfg.Auth)
 
-	data, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, `{"detail": "Failed to read data from body"}`, http.StatusBadRequest)
-		return
-	}
+	mysql := mysql.New(cfg.Mysql)
+	userSvc := userservice.New(authSvc, mysql)
 
-	var req userservice.RegisterRequest
-	err = json.Unmarshal(data, &req)
-	if err != nil {
-		fmt.Fprintf(w, `{"detail": "Failed to unmarshal data to json: %s"}`, err.Error())
-		return
-	}
-
-	authSvc := authservice.New(
-		JWTSignKey, AccessTokenSubject, RefreshTokenSubject, AccessTokenDuration, RefreshTokenDuration,
-	)
-	mysqlRepo := mysql.New()
-	userSvc := userservice.New(authSvc, mysqlRepo)
-
-	_, err = userSvc.Register(req)
-	if err != nil {
-		fmt.Fprintf(w, `{"detail": "%s"}`, err.Error())
-		return
-	}
-
-	w.Write([]byte(`{"detail": "User created successfully"}`))
-}
-
-func userLoginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, `{"detail": "Method not allowed"}`, http.StatusMethodNotAllowed)
-		return
-	}
-
-	data, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, `{"detail": "Failed to read data from body"}`, http.StatusBadRequest)
-		return
-	}
-
-	var lReq userservice.LoginRequest
-	err = json.Unmarshal(data, &lReq)
-	if err != nil {
-		fmt.Fprintf(w, `{"detail": "Failed to unmarshal data to json: %s"}`, err.Error())
-		return
-	}
-
-	authSvc := authservice.New(
-		JWTSignKey, AccessTokenSubject, RefreshTokenSubject, AccessTokenDuration, RefreshTokenDuration,
-	)
-	mysqlRepo := mysql.New()
-	userSvc := userservice.New(authSvc, mysqlRepo)
-
-	response, err := userSvc.Login(lReq)
-	if err != nil {
-		fmt.Fprintf(w, `{"detail": "%s"}`, err.Error())
-		return
-	}
-
-	data, mErr := json.Marshal(response)
-	if mErr != nil {
-		fmt.Fprintf(w, `{"detail": "%s"}`, mErr.Error())
-		return
-	}
-
-	w.Write(data)
-}
-
-func userProfileHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, `{"detail": "Method not allowed"}`, http.StatusMethodNotAllowed)
-		return
-	}
-
-	authSvc := authservice.New(
-		JWTSignKey, AccessTokenSubject, RefreshTokenSubject, AccessTokenDuration, RefreshTokenDuration,
-	)
-
-	authToken := r.Header.Get("Authorization")
-	claim, err := authSvc.ParseToken(authToken)
-	if err != nil {
-		http.Error(w, `{"detail": "Token is not valid"}`, http.StatusBadRequest)
-		return
-	}
-
-	mysqlRepo := mysql.New()
-	userSvc := userservice.New(authSvc, mysqlRepo)
-
-	resp, err := userSvc.Profile(userservice.ProfileRequest{UserID: claim.UserID})
-	if err != nil {
-		fmt.Fprintf(w, `{"detail": "%s"}`, err.Error())
-		return
-	}
-
-	data, err := json.Marshal(resp)
-	if err != nil {
-		fmt.Fprintf(w, `{"detail": "%s"}`, err.Error())
-		return
-	}
-
-	w.Write(data)
+	return authSvc, userSvc
 }
