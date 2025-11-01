251953480643141f56799e721bf0571ba3b23010
Sajjad fani <faniam321@gmail.com>
2025-11-01 19:58:26 +0330

    add error constants

---
+++ pkg/errmsg/message.go
@@ -0,0 +1,8 @@
+package errmsg
+
+const (
+	ErrorMsgNotFound           = "record not found"
+	ErrorMsgCantScanQueryScan  = "can't scan the result"
+	ErrorMsgCantExc            = "can not execute command"
+	ErrorMsgSomethingWentWrong = "something went wrong"
+)
